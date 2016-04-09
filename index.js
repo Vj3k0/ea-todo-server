2e440d122e4246421e6a38ae3b343ad14139065f
Vjekoslav Ratkajec <vratkajec@gmail.com>
2016-04-09 09:50:46 +0200

    Added manifest updating in interval of 5 minutes.

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 const http = require('http');
 const isDevelopment = process.env.NODE_ENV === 'development';
 
+// Fetch manifest info every 5 minutes
+const FETCH_INTERVAL = 300000;
+
 var manifest = null;
 
 app.use(require('morgan')('dev'));
@@ -43,22 +46,31 @@ let getBaseUrl = () => {
   }
 }
 
+let getManifest = () => {
+    console.log(`Checking manifest from ${manifestUrl}`);
+    http.get(manifestUrl, function(res){
+        var body = '';
+
+        res.on('data', function(chunk){
+            body += chunk;
+        });
+
+        res.on('end', function(){
+            var remoteManifest = JSON.parse(body);
+            if (!manifest || manifest.version !== remoteManifest.version) {
+                manifest = remoteManifest;
+                console.log(`Manifest loaded - latest v${manifest.version}`);
+            }
+        });
+    }).on('error', function(e){
+        console.log("Got an error: ", e);
+    });
+    
+    setTimeout(getManifest, FETCH_INTERVAL);
+}
+
 const manifestUrl = `${getBaseUrl()}/updates/releases/osx/manifest.json`;
-console.log(`Fetching manifest from ${manifestUrl}`);
-http.get(manifestUrl, function(res){
-    var body = '';
-
-    res.on('data', function(chunk){
-        body += chunk;
-    });
-
-    res.on('end', function(){
-        manifest = JSON.parse(body);
-        console.log(`Manifest loaded - latest v${manifest.version}`);
-    });
-}).on('error', function(e){
-      console.log("Got an error: ", e);
-});
+getManifest();
 
 app.listen(process.env.PORT, () => {
   console.log(`Express server listening on port ${process.env.PORT}`);
